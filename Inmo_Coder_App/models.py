cde3bea2b6e156e689bcc445c39e8b8c6e540f47
Federico Dalinger <fededalinger@gmail.com>
2022-08-30 03:57:54 -0300

    Modificamos la forma en que representa la informacion en la base de datos, entrando con admin.

--- Inmo_Coder_App/models.py
+++ Inmo_Coder_App/models.py
@@ -5,38 +5,47 @@ from django.db import models
 class Casas (models.Model):
     ubicacion = models.CharField(max_length=100)
     ambientes = models.IntegerField()
+    precio = models.IntegerField()
     
     contacto_nombre = models.CharField(max_length=100)
     contacto_telefono = models.IntegerField()
     contacto_email = models.EmailField()
-    
-    precio = models.IntegerField()
 
     fecha_de_alta = models.DateField()
+
+    def __str__(self):
+        #ESTO ES COMO VOY A VER MI BASE DE DATOS EN "ADMIN":
+        return "Ubicacion: " + self.ubicacion + " | Ambientes: " + str(self.ambientes) + " | Precio: " + str(self.precio) + " | Contacto -> Nombre: " + self.contacto_nombre + ", Teléfono: " + str(self.contacto_telefono) + ", E-mail: " + self.contacto_email + " | Fecha de publicación: " + str(self.fecha_de_alta)  + " |"
     
 
 class Departamentos (models.Model):
     ubicacion = models.CharField(max_length=100)
     ambientes = models.IntegerField()
+    precio = models.IntegerField()
     
     contacto_nombre = models.CharField(max_length=100)
     contacto_telefono = models.IntegerField()
     contacto_email = models.EmailField()
-    
-    precio = models.IntegerField()
 
     fecha_de_alta = models.DateField()
+
+    def __str__(self):
+        #ESTO ES COMO VOY A VER MI BASE DE DATOS EN "ADMIN":
+        return "Ubicacion: " + self.ubicacion + " | Ambientes: " + str(self.ambientes) + " | Precio: " + str(self.precio) + " | Contacto -> Nombre: " + self.contacto_nombre + ", Teléfono: " + str(self.contacto_telefono) + ", E-mail: " + self.contacto_email + " | Fecha de publicación: " + str(self.fecha_de_alta)  + " |"
 
 class Cocheras (models.Model):
     ubicacion = models.CharField(max_length=100)
+    precio = models.IntegerField() 
 
     contacto_nombre = models.CharField(max_length=100)
     contacto_telefono = models.IntegerField()
     contacto_email = models.EmailField()
     
-    precio = models.IntegerField() 
-
     fecha_de_alta = models.DateField()
+
+    def __str__(self):
+        #ESTO ES COMO VOY A VER MI BASE DE DATOS EN "ADMIN":
+        return "Ubicacion: " + self.ubicacion + " | Precio: " + str(self.precio) + " | Contacto -> Nombre: " + self.contacto_nombre + ", Teléfono: " + str(self.contacto_telefono) + ", E-mail: " + self.contacto_email + " | Fecha de publicación: " + str(self.fecha_de_alta)  + " |"
 
 
 class Clientes (models.Model):
@@ -49,3 +58,7 @@ class Clientes (models.Model):
     contacto_email = models.EmailField()
 
     fecha_de_alta = models.DateField()
+
+    def __str__(self):
+        #ESTO ES COMO VOY A VER MI BASE DE DATOS EN "ADMIN":
+        return "Contacto -> Nombre: " + self.contacto_nombre + ", Teléfono: " + str(self.contacto_telefono) + ", E-mail: " + self.contacto_email + "Motivo -> Detalle: " + self.motivo_descripcion + ", Ubicación: " + self.motivo_ubicacion + ", Precio: " + str(self.precio) + " | Fecha de publicación: " + str(self.fecha_de_alta)  + " |"
