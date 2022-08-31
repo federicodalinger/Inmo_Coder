76ad9fd29f8a43daacf22a82fe686cc59bc098e4
Federico La Volpe <XS00093@lumen.com>
2022-08-31 08:09:34 -0300

    Form casa y depto creados y graban en BD

--- Inmo_Coder_App/forms.py
+++ Inmo_Coder_App/forms.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from socket import fromshare
 from django import forms
+import datetime
 
 
 class Cargocasa(forms.Form):
@@ -9,4 +11,14 @@ class Cargocasa(forms.Form):
     c_contacto_nombre=forms.CharField(max_length=50)
     c_contacto_telefono=forms.IntegerField()
     c_contacto_email=forms.EmailField()
-    c_fecha_alta=forms.DateField()
+    c_fecha_alta=forms.DateField(initial=datetime.date.today) 
+
+
+class Deptocarga(forms.Form):
+    c_ubicacion=forms.CharField(max_length=50)
+    c_ambientes=forms.IntegerField()
+    c_precio=forms.IntegerField()
+    c_contacto_nombre=forms.CharField(max_length=50)
+    c_contacto_telefono=forms.IntegerField()
+    c_contacto_email=forms.EmailField()
+    c_fecha_alta=forms.DateField(initial=datetime.date.today)
--- Inmo_Coder_App/views.py
+++ Inmo_Coder_App/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from Inmo_Coder_App.forms import Cargocasa
-from Inmo_Coder_App.models import Casas
+from Inmo_Coder_App.forms import Cargocasa,Deptocarga
+from Inmo_Coder_App.models import Casas,Departamentos
 
 # Create your views here.
 
@@ -15,7 +15,6 @@ def casas_cargar (request):
         miformulario = Cargocasa(request.POST)
         if miformulario.is_valid():
             info = miformulario.cleaned_data
-            print(info)
             v_ubicacion=info.get("c_ubicacion")
             v_ambientes=info.get("c_ambientes")
             v_precio=info.get("c_precio")
@@ -23,11 +22,13 @@ def casas_cargar (request):
             v_telefono=info.get("c_contacto_telefono")
             v_email=info.get("c_contacto_email")
             v_fecha=info.get("c_fecha_alta")
-
+            print("Entro")
                         
             casa = Casas(ubicacion=v_ubicacion,ambientes=v_ambientes,precio=v_precio,contacto_nombre=v_contacto,contacto_telefono=v_telefono,contacto_email=v_email,fecha_de_alta=v_fecha)
             casa.save()
-            return render(request,"Inmo_Coder_App/templates/Inmo_Coder_App/inicio.html")
+            return render(request,"Inmo_Coder_App/templates/Inmo_Coder_App/inicio.html",{"mensaje":"Casa creada"})
+        else:
+            return render(request,"Inmo_Coder_App/templates/Inmo_Coder_App/inicio.html",{"mensaje":"Error en la carga del form"})
     else:
         miformulario=Cargocasa()
         print("hola")
@@ -41,7 +42,30 @@ def casas_buscar (request):
 
 ################# Views referida a DEPARTAMENTOS: ################
 def departamentos_cargar (request):
-    return render (request, "Inmo_Coder_App/departamentos_cargar.html")
+    if request.method == "POST":
+        miformulario = Deptocarga(request.POST)
+        if miformulario.is_valid():
+            info = miformulario.cleaned_data
+            v_ubicacion=info.get("c_ubicacion")
+            v_ambientes=info.get("c_ambientes")
+            v_precio=info.get("c_precio")
+            v_contacto=info.get("c_contacto_nombre")
+            v_telefono=info.get("c_contacto_telefono")
+            v_email=info.get("c_contacto_email")
+            v_fecha=info.get("c_fecha_alta")
+
+                        
+            casa = Departamentos(ubicacion=v_ubicacion,ambientes=v_ambientes,precio=v_precio,contacto_nombre=v_contacto,contacto_telefono=v_telefono,contacto_email=v_email,fecha_de_alta=v_fecha)
+            casa.save()
+            return render(request,"Inmo_Coder_App/templates/Inmo_Coder_App/inicio.html",{"mensaje":"Departamento Creado"})
+        else:
+            return render(request,"Inmo_Coder_App/templates/Inmo_Coder_App/inicio.html",{"mensaje":"Error en creacion de departamento"})
+    else:
+        miformulario=Deptocarga()
+        print("hola")
+        return render(request,"Inmo_Coder_App/templates/Inmo_Coder_App/departamentos_cargar.html", {"miformulario":miformulario})
+
+
 
 def departamentos_buscar (request):
     return render (request, "Inmo_Coder_App/departamentos_buscar.html")
