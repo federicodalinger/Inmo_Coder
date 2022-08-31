3235c79d31d092238cf6607e2f4a272be0519612
Feli Goico <felipe.g1901@hotmail.com>
2022-08-30 21:52:45 -0400

    Empiezo con las views

--- Inmo_Coder_App/views.py
+++ Inmo_Coder_App/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import *
 from Inmo_Coder_App.forms import CocherasFormulario, ClientesFormulario
 
 # Create your views here.
@@ -25,6 +26,22 @@ def departamentos_buscar (request):
 
 ################# Views referida a COCHERAS: ################
 def cocheras_cargar (request):
+
+    if request.method == 'POST':
+
+        form_cocheras = CocherasFormulario(request.POST)
+
+        if form_cocheras.is_valid:
+
+            informacion = form_cocheras.cleaned_data
+
+            cochera = Cocheras(ubicacion=informacion['ubicacion'], precio=informacion['precio'], contacto_nombre=informacion['contacto_nombre'], contacto_telefono=informacion['contacto_telefono'], contacto_email=informacion['contacto_email'], fecha_de_alta=informacion['fecha_de_alta'])
+
+            cochera.save()
+
+    else:
+        form_cocheras = CocherasFormulario()
+
     return render (request, "Inmo_Coder_App/cocheras_cargar.html")
 
 def cocheras_buscar (request):
@@ -38,14 +55,3 @@ def clientes_cargar (request):
 def clientes_buscar (request):
     return render (request, "Inmo_Coder_App/clientes_buscar.html")
 
-
-
-    '''    if request.method == 'POST':
-
-        form_cocheras = CocherasFormulario(request.POST)
-
-        if form_cocheras.is_valid:
-
-            informacion = form_cocheras.cleaned_data
-
-            cochera = Cocheras '''
