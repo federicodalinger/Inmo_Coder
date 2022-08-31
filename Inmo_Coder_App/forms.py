c4eaf9b471ea2d22bc97ef8da6975fb7604cabaf
Feli Goico <felipe.g1901@hotmail.com>
2022-08-30 21:08:14 -0400

    Creo los forms y los importo en las views

---
+++ Inmo_Coder_App/forms.py
@@ -0,0 +1,19 @@
+from socket import fromshare
+from django import forms
+
+class CocherasFormulario(forms.Form):
+    ubicacion = forms.CharField(max_length=100)
+    precio = forms.IntegerField() 
+    contacto_nombre = forms.CharField(max_length=100)
+    contacto_telefono = forms.IntegerField()
+    contacto_email = forms.EmailField()
+    fecha_de_alta = forms.DateField()
+
+class ClientesFormulario(forms.Form):
+    motivo_descripcion = forms.CharField(max_length=100) # Esto seria el motivo por el cual es nuestro cliente (por ejemplo: "venta de.../compra de.../alquiler de...").
+    motivo_ubicacion = forms.CharField(max_length=100) # Aca indicamos la direccion de la operacion previa.
+    motivo_precio = forms.IntegerField() # El monto por el cual se firmo la venta, compra, alquiler.
+    contacto_nombre = forms.CharField(max_length=100)
+    contacto_telefono = forms.IntegerField()
+    contacto_email = forms.EmailField()
+    fecha_de_alta = forms.DateField()
